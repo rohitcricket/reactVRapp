7789c4d5f9857db3c045800f7095cf1285521bf9
Rohit Gupta <rohitcricket@gmail.com>
2019-06-25 18:51:09 -0700

    added flag colors

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ import {
 
 export default class travelVR extends React.Component {
   render() {
+    const { mainView, menuItem, poland, ukraine, uk, spain, italy, greece} = styles;
     return (
-      <View style={styles.mainView}>
-        <Text style={styles.menuItem}>Poland</Text>
-        <Text style={styles.menuItem}>Ukraine</Text>
-        <Text style={styles.menuItem}>Great Britain</Text>
-        <Text style={styles.menuItem}>Spain</Text>
-        <Text style={styles.menuItem}>Italy</Text>
-        <Text style={styles.menuItem}>Greece</Text>
+      <View style={mainView}>
+        <Text style={[menuItem, poland]}>Poland</Text>
+        <Text style={[menuItem, ukraine]}>Ukraine</Text>
+        <Text style={[menuItem, uk]}>Great Britain</Text>
+        <Text style={[menuItem, spain]}>Spain</Text>
+        <Text style={[menuItem, italy]}>Italy</Text>
+        <Text style={[menuItem, greece]}>Greece</Text>
       </View>
     )
 
@@ -27,7 +28,9 @@ const styles = StyleSheet.create({
     height: 600,
     width: 600,
     padding: 10,
-    backgroundColor: "#eee"
+    backgroundColor: "#eee",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   menuItem: {
     fontSize: 40,
@@ -35,6 +38,24 @@ const styles = StyleSheet.create({
     marginTop: 5,
     backgroundColor: '#0298D0',
     color: 'white'
+  },
+  poland: { 
+    backgroundColor: '#DC143C'
+  },
+  ukraine: {
+    backgroundColor: '#FFD500'
+  },
+  uk: {
+    backgroundColor: '#00247D'
+  },
+  spain: {
+    backgroundColor: '#C60D1F'
+  },
+  italy: {
+    backgroundColor: '#029246'
+  },
+  greece: {
+    backgroundColor: '#0D5EAF'
   }
 });
 
